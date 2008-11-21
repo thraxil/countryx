87b769eb0efa000f4684f82fbca46badbf005219
sdreher <sdreher@1f418930-7ff8-0310-b8ad-c653122473bc>
2008-11-21 15:51:14 +0000

    Finishing up login unit tests. Checking in the initial_data fixture for the tests
    git-svn-id: http://svn.ccnmtl.columbia.edu/genocideprevention/trunk@12924 1f418930-7ff8-0310-b8ad-c653122473bc

--- sim/tests.py
+++ sim/tests.py
@@ -1,57 +1,63 @@
 from django.test.client import Client
 from django.test import TestCase
 from genocideprevention.sim.models import *
-from django.utils.translation import ugettext_lazy as _
 
 class ViewTestCase(TestCase):
-    fixtures = ['testdata']
-    
-    def test_fixture(self):
-        role = Role.objects.get(name="President")
-        self.assert_(role)
 
-#    def test_index_not_logged_in(self):
-#        # Issue a GET request for the index page
-#        client = Client()
-#        response = client.get('/sim/')
-#        
-#        # Check that the response is 200 OK and that we're on the login page
-#        self.assertContains(response, "login", status_code=200)
-#
-#        # Check that the context is what we expect
-#        self.failUnlessEqual(len(response.context[0]["error_message"]), 0)
-#        self.assertTemplateUsed(response, "sim/login.html")
-
-#    def test_django_login_no_cookies(self):
-#        #The test fixture does not support the test cookie setting.
-#        #exploiting this to test the "no cookies" path
-#                
-#        # Issue a POST request for the login page that has the required username,password supplied
-#        response = self.client.post('/sim/login/', {'username': 'testuser', 'password': 'test'})
-#        
-#        # Check that the response is 200 OK and that we're on the login page
-#        self.assertContains(response, "login", status_code=200)
-#        self.assert_(response.context[0]["error_message"].find("enable cookies") > 0)
-#        
-#    def test_django_login(self):
-#        # Do a fake login via the handy client login fixture
-#        c = Client()
-#        uname = u'testuser'
-#        pwd = u'test'
-#        self.assert_(c.login(username=uname, password=pwd))
-#        
-#        response = c.get('/sim/')
-#        
-#        # Check that the response is 200 OK and that we're on the login page
-#        print response.content
-#        self.assertContains(response, "Welcome", status_code=200)
-#
-#        # Check that the context is what we expect
-#        self.failUnlessEqual(len(response.context[0]["error_message"]), 0)
-#        self.assertTemplateUsed(response, "sim/index.html")
+    def test_index_not_logged_in(self):
+        # Issue a GET request for the index page
+        client = Client()
+        response = client.get('/sim/')
         
+        self.assertRedirects(response, expected_url="/accounts/login/?next=/sim/", status_code=302, target_status_code=200)
+                
+    def test_django_login(self):
+        client = Client()
+        
+        # Issue a POST request for the login page that has the required username,password supplied
+        response = client.post('/accounts/login/', {'username': 'testuser', 'password': 'test', "next": "/sim/"})
+        
+        self.assertRedirects(response, expected_url="/sim/", status_code=302, target_status_code=200)
+        
+        # Now that the user is logged in, verify they are sent to the right place
+        response = client.get('/sim/')
+        self.assertContains(response, "Welcome", status_code=200)
+        self.assertTemplateUsed(response, "sim/index.html")
+        
+    def test_django_invalid_password(self):
+        client = Client()
+                
+        # Issue a POST request for the login page that has the required username,password supplied
+        response = client.post('/accounts/login/', {'username': 'testuser', 'password': 'foo', "next": "/sim/"})
+                 
+        self.assertContains(response, "Your username and password didn't match", status_code=200)
+        self.assertTemplateUsed(response, "registration/login.html")
+        
+        self.assertFormError(response, "form", "__all__", "Please enter a correct username and password. Note that both fields are case-sensitive.")
+        
+    def test_django_already_loggedin(self):
+        # Do a fake login via the handy client login fixture
+        c = Client()
+        uname = u'testuser'
+        pwd = u'test'
+        self.assert_(c.login(username=uname, password=pwd))
+        
+        response = c.get('/sim/')
+        self.assertContains(response, "Welcome", status_code=200)
+        self.assertTemplateUsed(response, "sim/index.html")
 
-  
+    def test_django_logout(self):
+        # Do a fake login via the handy client login fixture
+        c = Client()
+        self.assert_(c.login(username=u'testuser', password=u'test'))
+        
+        response = c.get('/sim/logout/')
+        self.assertContains(response, "logged out", status_code=200)   
+        self.assertTemplateUsed(response, "sim/logged_out.html")     
+        
+   
+        
+        
         
         
         
--- sim/views.py
+++ sim/views.py
@@ -2,22 +2,14 @@ from django.template import Context, loader
 from genocideprevention.sim.models import *
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.template import Context, loader
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
 
+@login_required
 def root(request):
-    if (not request.user.is_authenticated()):
-        return HttpResponseRedirect('login/')
-    else:
-        return index(request)
+   return render_to_response("sim/index.html",dict(user=request.user))
 
-def index(request):
-    t = loader.get_template('sim/index.html')
-    
-    c = Context({
-       'user': request.user,
-    })
-    
-    return HttpResponse(t.render(c))
-
+@login_required
 def narrative(request, group_id, player_id):
     t = loader.get_template('sim/narrative.html')
     
@@ -35,6 +27,7 @@ def narrative(request, group_id, player_id):
     
     return HttpResponse(t.render(c))
 
+@login_required
 def decision(request, group_id, player_id):
     t = loader.get_template('sim/decision.html')
     
@@ -63,5 +56,4 @@ def __current_conditions(group):
     conditions.append(group.current_state.statevariable_set.get(name='Political Discourse'))
     conditions.append(group.current_state.statevariable_set.get(name='Weapons Flow'))
     return conditions
-
    
