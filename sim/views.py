9b5363061166ab019654fdd460d0eee044fe2149
Anders Pearson <anders@columbia.edu>
2013-01-14 09:43:49 -0500

    unused import

--- sim/views.py
+++ sim/views.py
@@ -8,7 +8,6 @@ from countryx.sim.models import StateChange, StateVariable
 from countryx.sim.models import SectionGroupState
 from countryx.sim.models import get_or_create_user
 from countryx.sim.models import get_or_create_section
-from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
