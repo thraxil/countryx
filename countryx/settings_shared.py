73825030fe560e38dd5e897228874032324e4f83
Anders Pearson <anders@columbia.edu>
2013-01-15 10:27:11 -0500

    add django-debug-toolbar

--- countryx/settings_shared.py
+++ countryx/settings_shared.py
@@ -71,6 +71,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
     'countryx.sim.middleware.GameStateMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'countryx.urls'
@@ -93,6 +94,20 @@ INSTALLED_APPS = (
     'django_nose',
     'south',
     'raven.contrib.django',
+    'debug_toolbar',
+)
+
+INTERNAL_IPS = ('127.0.0.1', )
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 SOUTH_TESTS_MIGRATE = False
