319f2247b9cd93006784e026541e14e66f4b7fe7
Anders Pearson <anders@columbia.edu>
2014-06-26 10:54:18 -0400

    oops. hadn't disabled indent-tabs-mode in emacs for javascript

--- media/js/game.js
+++ media/js/game.js
@@ -3,27 +3,27 @@ var gCurrentChoice = 0;
 function debug(string) {
    if (true) {
       log(string);
-	 }
+   }
 }
 
 function toggleCharacterProfile(control, characterName) {
-		debug("toggle: " + characterName);
+    debug("toggle: " + characterName);
 
-		desc = $(characterName + "_description");
-		img = $(characterName + "_image");
+    desc = $(characterName + "_description");
+    img = $(characterName + "_image");
 
-		toggle(desc, 'blind');
-		// nit: have to add the overflow style back for text field.
-		// it stays set as 'hidden'
-		setStyle(desc, {'overflow': 'auto'});
+    toggle(desc, 'blind');
+    // nit: have to add the overflow style back for text field.
+    // it stays set as 'hidden'
+    setStyle(desc, {'overflow': 'auto'});
 
-		toggle(img, 'slide');
+    toggle(img, 'slide');
 
-		if ('none' == getStyle(desc, 'display')) {
-				control.innerHTML = 'Hide Profile';
-		} else {
-				control.innerHTML = 'Show Profile';
-		}
+    if ('none' == getStyle(desc, 'display')) {
+        control.innerHTML = 'Hide Profile';
+    } else {
+        control.innerHTML = 'Show Profile';
+    }
 }
 
 function clearChoice(control) {
@@ -37,7 +37,7 @@ function clearChoice(control) {
            function(elem) {
                if (elem.id != gCurrentChoice && 'none' == getStyle(elem, 'display')) {
                   toggle(elem, effect);
-							 }
+               }
            });
 
    // hide the feedback form underneath
@@ -45,22 +45,22 @@ function clearChoice(control) {
 }
 
 function choose(control, choice) {
-		if (gCurrentChoice === 0) {
-				gCurrentChoice = choice;
-				effect = 'blind';
+    if (gCurrentChoice === 0) {
+        gCurrentChoice = choice;
+        effect = 'blind';
 
-				// hide the other choices, leaving the one the user chose
-				elements = getElementsByTagAndClassName(null, "player_choice");
-				forEach(elements,
-								function(elem) {
-										if (elem.id != choice) {
-												toggle(elem, effect);
-										}
-								});
+        // hide the other choices, leaving the one the user chose
+        elements = getElementsByTagAndClassName(null, "player_choice");
+        forEach(elements,
+                function(elem) {
+                    if (elem.id != choice) {
+                        toggle(elem, effect);
+                    }
+                });
 
-				// show the feedback form underneath
-				toggle($('reasoning_form'), effect);
-		}
+        // show the feedback form underneath
+        toggle($('reasoning_form'), effect);
+    }
 }
 
 function clearMessages() {
@@ -70,71 +70,71 @@ function clearMessages() {
 }
 
 function saveChoiceSuccess(response) {
-		doc = JSON.parse(response.responseText, null);
+    doc = JSON.parse(response.responseText, null);
     
-		if (doc.result === 0) {
-				$('errorMsg').innerHTML = "An error occurred while saving your choices. Please logout and log back in again and try to resubmit.";
-				setStyle($('errorMsg'), {'display': 'block'});
-		} else if (doc.result == 1) {
-				// draft -- leave the screen the way it was
-				$('successMsg').innerHTML = "Your choices have been saved.";
-				setStyle($('successMsg'), {'display': 'block'});
+    if (doc.result === 0) {
+        $('errorMsg').innerHTML = "An error occurred while saving your choices. Please logout and log back in again and try to resubmit.";
+        setStyle($('errorMsg'), {'display': 'block'});
+    } else if (doc.result == 1) {
+        // draft -- leave the screen the way it was
+        $('successMsg').innerHTML = "Your choices have been saved.";
+        setStyle($('successMsg'), {'display': 'block'});
           
-				// enable the buttons
-				$('savedraft').disabled = false;
-				$('submit').disabled = false;
-				$('clear').disabled = false;
-		} else if (doc.result == 2) {
-				// submit succeeded -- hide the selection divs
+        // enable the buttons
+        $('savedraft').disabled = false;
+        $('submit').disabled = false;
+        $('clear').disabled = false;
+    } else if (doc.result == 2) {
+        // submit succeeded -- hide the selection divs
 
-				// submit -- leave the screen the way it was
-				$('successMsg').innerHTML = "Your choices have been submitted.";
-				setStyle($('successMsg'), {'display': 'block'});
+        // submit -- leave the screen the way it was
+        $('successMsg').innerHTML = "Your choices have been submitted.";
+        setStyle($('successMsg'), {'display': 'block'});
 
-				$(gCurrentChoice).onclick = '';
-				setNodeAttribute($('reasoning'), 'readonly', 'true');
-				setStyle($('savedraft'), {'display': 'none'});
-				setStyle($('submit'), {'display': 'none'});
-				setStyle($('clear'), {'display': 'none'});
-				$('submit_state_desc').innerHTML = "Final Submission";
-		}
+        $(gCurrentChoice).onclick = '';
+        setNodeAttribute($('reasoning'), 'readonly', 'true');
+        setStyle($('savedraft'), {'display': 'none'});
+        setStyle($('submit'), {'display': 'none'});
+        setStyle($('clear'), {'display': 'none'});
+        $('submit_state_desc').innerHTML = "Final Submission";
+    }
 }
 
 function saveChoiceError(err) {
-		$('errorMsg').innerHTML = err + ". This is probably a temporary error. We recommend that you log out and log back in again and try to resubmit.";
-		setStyle($('errorMsg'), {'display': 'block'});
+    $('errorMsg').innerHTML = err + ". This is probably a temporary error. We recommend that you log out and log back in again and try to resubmit.";
+    setStyle($('errorMsg'), {'display': 'block'});
 }
 
 function saveChoice(control, finalsubmit) {
-		clearMessages();
+    clearMessages();
 
-		if (finalsubmit && $('reasoning').value.length < 1) {
-				$('errorClient').innerHTML = "Please enter your reasoning before submitting a final answer.";
-				setStyle($('errorClient'), {'display': 'block'});
-		} else if (finalsubmit && !confirm("Are you sure you're ready to submit your choice and reasoning? Your responses are final once you hit OK")) {
-				return; // do nothing
-		} else if (gCurrentChoice === 0) {
-				alert("DEBUG: Current Choice is 0");
-		} else {
-				// disable the buttons
-				$('savedraft').disabled = true;
-				$('submit').disabled = true;
-				$('clear').disabled = true;
+    if (finalsubmit && $('reasoning').value.length < 1) {
+        $('errorClient').innerHTML = "Please enter your reasoning before submitting a final answer.";
+        setStyle($('errorClient'), {'display': 'block'});
+    } else if (finalsubmit && !confirm("Are you sure you're ready to submit your choice and reasoning? Your responses are final once you hit OK")) {
+        return; // do nothing
+    } else if (gCurrentChoice === 0) {
+        alert("DEBUG: Current Choice is 0");
+    } else {
+        // disable the buttons
+        $('savedraft').disabled = true;
+        $('submit').disabled = true;
+        $('clear').disabled = true;
        
-				parts = location.href.split('/');
-				groupid = parts[parts.length - 2];
-				url = 'http://' + location.hostname + ':' + location.port + "/sim/player/choose/";
-				deferred = doXHR(
-						url,
-						{
-								method: 'POST',
-								sendContent: queryString({'groupid': groupid, 'choiceid': gCurrentChoice, 'final': finalsubmit, 'reasoning': $('reasoning').value})
-						});
+        parts = location.href.split('/');
+        groupid = parts[parts.length - 2];
+        url = 'http://' + location.hostname + ':' + location.port + "/sim/player/choose/";
+        deferred = doXHR(
+            url,
+            {
+                method: 'POST',
+                sendContent: queryString({'groupid': groupid, 'choiceid': gCurrentChoice, 'final': finalsubmit, 'reasoning': $('reasoning').value})
+            });
         deferred.addCallbacks(saveChoiceSuccess, saveChoiceError);
-		}
+    }
 }
 
 function initializeGame() {
-   gCurrentChoice = $('current_choice').innerHTML;
+    gCurrentChoice = $('current_choice').innerHTML;
 }
 MochiKit.Signal.connect(window, "onload", initializeGame);
