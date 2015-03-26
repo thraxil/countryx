b9980e2183ca90137d4ece79e37931b95e6f9906
Anders Pearson <anders@columbia.edu>
2015-03-26 15:20:08 +0100

    move to method

--- countryx/sim/models.py
+++ countryx/sim/models.py
@@ -289,6 +289,27 @@ class Section(models.Model):
                 state=start_state,
                 date_updated=datetime.datetime.now())
 
+    def ensure_consistency(self):
+        section_turn = self.current_turn()
+        # verify each group's current state == the current section turn
+        # if not, then add the state to the group and make sure all
+        # members get automated answers.
+        groups = self.sectiongroup_set.all()
+        for group in groups:
+            try:
+                group_state = (group.sectiongroupstate_set.latest()
+                               .state)
+            except SectionGroupState.DoesNotExist:
+                group_state = State.objects.get(state_no=1, turn=1)
+                SectionGroupState.objects.create(
+                    state=group_state, group=group,
+                    date_updated=datetime.datetime.now())
+            if (section_turn != group_state.turn):
+                group.force_response_all_players()
+                # update the group state to the next turn based on
+                # the player choices
+                group.update_state()
+
 
 class SectionAdministrator(models.Model):
     user = models.ForeignKey(User)
