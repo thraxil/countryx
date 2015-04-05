d2d6c4f1a08eb44bbeef29c5e0b6e9d2abb14a6b
Anders Pearson <anders@columbia.edu>
2015-04-05 16:15:55 +0200

    cloudfront

--- countryx/settings_staging.py
+++ countryx/settings_staging.py
@@ -24,13 +24,14 @@ DATABASES = {
 }
 STATSD_PREFIX = 'countryx-staging'
 
+AWS_S3_CUSTOM_DOMAIN = 'dp0pzu8v53cf7.cloudfront.net'
 AWS_STORAGE_BUCKET_NAME = "ccnmtl-countryx-static-stage"
 AWS_PRELOAD_METADATA = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
 # static data, e.g. css, js, etc.
 STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_ROOT = STATIC_ROOT
