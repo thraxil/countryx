40204e28d2904bee19fe436b1f4a38be968dc21f
Anders Pearson <anders@columbia.edu>
2015-04-05 15:38:34 +0200

    compress offline needs correct template dirs

--- countryx/settings_production.py
+++ countryx/settings_production.py
@@ -1,8 +1,9 @@
 # flake8: noqa
 from settings_shared import *
+import os
 
 TEMPLATE_DIRS = (
-    "/var/www/countryx/countryx/countryx/templates",
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 MEDIA_ROOT = '/var/www/countryx/uploads/'
