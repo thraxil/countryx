a4c0286f2f8a92d98419bdeda55e0eaf4f146d0b
Anders Pearson <anders@columbia.edu>
2015-04-26 18:34:37 +0200

    remove unused cheat functionality

--- media/js/cheat.js
+++
@@ -1,7 +0,0 @@
-function selectState(turn,stateId,stateName,color) {
-  $("selectedStateName").innerHTML = stateName;
-  $('id_turn').value = turn;
-  $('id_state').value = stateId;
-  setStyle($("selectedStateName").parentNode,{ 'background-color' : color	});
-  $('submitbutton').disabled = false;
-}
