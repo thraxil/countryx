a0be7554b713e3da64e22181c092829d7e16b904
Anders Pearson <anders@columbia.edu>
2015-04-27 11:16:49 +0200

    test a few more views

--- countryx/sim/tests/test_views.py
+++ countryx/sim/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, RequestFactory, Client
 
 from .factories import UserFactory, SectionAdministratorFactory
-from countryx.sim.views import root
+from countryx.sim.views import root, allpaths, allquestions, allvariables
 
 
 class RootTest(TestCase):
@@ -18,6 +18,39 @@ class RootTest(TestCase):
         request = self.factory.get("/")
         request.user = SectionAdministratorFactory().user
         response = root(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class AllPathsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_allpaths_empty(self):
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = allpaths(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class AllQuestionsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_allquestions_empty(self):
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = allquestions(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class AllVariablesTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_allpaths_empty(self):
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = allvariables(request)
         self.assertEqual(response.status_code, 200)
 
 
