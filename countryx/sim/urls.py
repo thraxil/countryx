d5d3b75f7a9e299cd487e035dc68ee3d78cd9e9b
Anders Pearson <anders@columbia.edu>
2016-08-02 16:38:49 +0100

    eliminate django.conf.urls.patterns
    deprecated in Django 1.9, removed in 1.10

--- countryx/sim/urls.py
+++ countryx/sim/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 import os.path
 from .views import (
     CreateSectionView, DeleteSectionView,
@@ -11,46 +11,45 @@ from .views import (
 
 media_root = os.path.join(os.path.dirname(__file__), "media")
 
-urlpatterns = patterns(
-    '',
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': media_root}),
-    (r'^$', 'countryx.sim.views.root'),
-    (r'^logout/$', 'django.contrib.auth.views.logout',
-     {'template_name': 'sim/logged_out.html'}),
+urlpatterns = [
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': media_root}),
+    url(r'^$', 'countryx.sim.views.root'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'sim/logged_out.html'}),
 
     # player pages
-    (r'^player/game/(?P<group_id>\d+)/$', 'countryx.sim.views.player_game'),
-    (r'^player/game/(?P<group_id>\d+)/(?P<turn_id>\d+)/$',
-     'countryx.sim.views.player_game'),
+    url(r'^player/game/(?P<group_id>\d+)/$', 'countryx.sim.views.player_game'),
+    url(r'^player/game/(?P<group_id>\d+)/(?P<turn_id>\d+)/$',
+        'countryx.sim.views.player_game'),
 
     # player ajax requests
-    (r'^player/choose/$', 'countryx.sim.views.player_choose'),
+    url(r'^player/choose/$', 'countryx.sim.views.player_choose'),
 
     # faculty ajax requests
-    (r'^faculty/reset/(?P<section_id>\d+)/$',
-     'countryx.sim.views.faculty_section_reset'),
+    url(r'^faculty/reset/(?P<section_id>\d+)/$',
+        'countryx.sim.views.faculty_section_reset'),
 
     # faculty management pages
-    (r'^allpaths/$', 'countryx.sim.views.allpaths'),
-    (r'^allpaths/questions$', 'countryx.sim.views.allquestions'),
-    (r'^allpaths/variables$', 'countryx.sim.views.allvariables'),
-    (r'^faculty/manage/(?P<section_id>\d+)/$',
-     'countryx.sim.views.faculty_section_manage'),
-    (r'^faculty/manage/(?P<section_id>\d+)/end_turn/$',
-     'countryx.sim.views.faculty_end_turn'),
-    (r'^faculty/groups/(?P<section_id>\d+)/$',
-     'countryx.sim.views.faculty_section_bygroup'),
-    (r'^faculty/players/(?P<section_id>\d+)/$',
-     'countryx.sim.views.faculty_section_byplayer'),
-    (r'^faculty/group/(?P<group_id>\d+)/$',
-     'countryx.sim.views.faculty_group_detail'),
-    ((r'^faculty/player/turn/(?P<group_id>\d+)/'
-      r'(?P<player_id>\d+)/(?P<state_id>\d+)/$'),
-     'countryx.sim.views.faculty_player_detail_byturn'),
-    (r'^faculty/player/(?P<player_id>\d+)/$',
-     'countryx.sim.views.faculty_player_detail'),
-    (r'^faculty/feedback/$', 'countryx.sim.views.faculty_feedback_submit'),
+    url(r'^allpaths/$', 'countryx.sim.views.allpaths'),
+    url(r'^allpaths/questions$', 'countryx.sim.views.allquestions'),
+    url(r'^allpaths/variables$', 'countryx.sim.views.allvariables'),
+    url(r'^faculty/manage/(?P<section_id>\d+)/$',
+        'countryx.sim.views.faculty_section_manage'),
+    url(r'^faculty/manage/(?P<section_id>\d+)/end_turn/$',
+        'countryx.sim.views.faculty_end_turn'),
+    url(r'^faculty/groups/(?P<section_id>\d+)/$',
+        'countryx.sim.views.faculty_section_bygroup'),
+    url(r'^faculty/players/(?P<section_id>\d+)/$',
+        'countryx.sim.views.faculty_section_byplayer'),
+    url(r'^faculty/group/(?P<group_id>\d+)/$',
+        'countryx.sim.views.faculty_group_detail'),
+    url((r'^faculty/player/turn/(?P<group_id>\d+)/'
+         r'(?P<player_id>\d+)/(?P<state_id>\d+)/$'),
+        'countryx.sim.views.faculty_player_detail_byturn'),
+    url(r'^faculty/player/(?P<player_id>\d+)/$',
+        'countryx.sim.views.faculty_player_detail'),
+    url(r'^faculty/feedback/$', 'countryx.sim.views.faculty_feedback_submit'),
     url(r'^section/new/$', CreateSectionView.as_view(), name='create-section'),
     url(r'^section/(?P<pk>\d+)/delete/$', DeleteSectionView.as_view(),
         name='delete-section'),
@@ -80,4 +79,4 @@ urlpatterns = patterns(
     url(r'^statechange/(?P<pk>\d+)/delete/$',
         StateChangeDelete.as_view(),
         name="delete-statechange"),
-)
+]
--- countryx/urls.py
+++ countryx/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import TemplateView
@@ -12,29 +12,24 @@ admin_media_root = os.path.join(
     os.path.dirname(__file__),
     "ve/lib/python2.6/site-packages/django/contrib/admin/media")
 
-urlpatterns = patterns(
-    '',
-    (r'^$', 'countryx.sim.views.root'),
-    (r'^accounts/logout/$',
-     'django.contrib.auth.views.logout', {'next_page': '/'}),
-    (r'^accounts/', include('django.contrib.auth.urls')),
-    (r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^$', 'countryx.sim.views.root'),
+    url(r'^accounts/logout/$',
+        'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^impersonate/', include('impersonate.urls')),
-    (r'^sim/', include('countryx.sim.urls')),
-    (r'^smoketest/', include('smoketest.urls')),
-    ('^stats/', TemplateView.as_view(template_name='stats.html')),
-    (r'^site_media/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': site_media_root}),
-    (r'^admin_media/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': admin_media_root}),
-    (r'^uploads/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-)
-
-urlpatterns += patterns(
-    'django.views.generic.simple',
-    (r'^about', TemplateView.as_view(template_name='flatpages/about.html')),
-    (r'^help', TemplateView.as_view(template_name='flatpages/help.html')),
-    (r'^contact',
-     TemplateView.as_view(template_name='flatpages/contact.html')),
-)
+    url(r'^sim/', include('countryx.sim.urls')),
+    url(r'^smoketest/', include('smoketest.urls')),
+    url('^stats/', TemplateView.as_view(template_name='stats.html')),
+    url(r'^site_media/(?P<path>.*)$',
+        'django.views.static.serve', {'document_root': site_media_root}),
+    url(r'^admin_media/(?P<path>.*)$',
+        'django.views.static.serve', {'document_root': admin_media_root}),
+    url(r'^uploads/(?P<path>.*)$',
+        'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^about', TemplateView.as_view(template_name='flatpages/about.html')),
+    url(r'^help', TemplateView.as_view(template_name='flatpages/help.html')),
+    url(r'^contact',
+        TemplateView.as_view(template_name='flatpages/contact.html')),
+]
