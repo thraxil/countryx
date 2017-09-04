89d1db3e074e5ff5b8843d75040a0e28b3787285
Anders Pearson <anders@thraxil.org>
2017-09-04 22:19:41 +0100

    super simple filtering of events

--- countryx/events/urls.py
+++ countryx/events/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.ListEvents.as_view(), name='events-list'),
     url(r'^event/(?P<pk>\d+)/$', views.EventDetail.as_view(),
         name='event-detail'),
+    url(r'^filter/$', views.FieldValueFilter.as_view(),
+        name='field-value-filter'),
 ]
--- countryx/events/views.py
+++ countryx/events/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, View
 
-from .models import Event
+from .models import Event, EventField
 
 from countryx.sim.views import StaffOnlyMixin
 
@@ -15,3 +16,16 @@ class ListEvents(StaffOnlyMixin, ListView):
 
 class EventDetail(StaffOnlyMixin, DetailView):
     model = Event
+
+
+class FieldValueFilter(StaffOnlyMixin, View):
+    template_name = "events/field_value_filter.html"
+
+    def get(self, request):
+        field = request.GET.get('field')
+        value = request.GET.get('value')
+        fields = EventField.objects.filter(
+            name=field, value=value
+        ).order_by('-event__timestamp')[:200]
+        return render(request, self.template_name,
+                      dict(fields=fields, field=field, value=value))
