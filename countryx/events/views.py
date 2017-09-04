e5b74de44810951389e880e820f672fa7ebc9888
Anders Pearson <anders@thraxil.org>
2017-09-04 21:13:52 +0100

    paginate events list

--- countryx/events/views.py
+++ countryx/events/views.py
@@ -7,6 +7,7 @@ from countryx.sim.views import StaffOnlyMixin
 
 class ListEvents(StaffOnlyMixin, ListView):
     model = Event
+    paginate_by = 200
 
     def get_queryset(self):
         return Event.objects.all().order_by('-timestamp')
