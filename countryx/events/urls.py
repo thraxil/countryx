99ea9e3c7d678dc94c7b370f55e53983c9424a3b
Anders Pearson <anders@thraxil.org>
2017-09-04 22:00:35 +0100

    event detail view

--- countryx/events/urls.py
+++ countryx/events/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^$', views.ListEvents.as_view())
+    url(r'^$', views.ListEvents.as_view(), name='events-list'),
+    url(r'^event/(?P<pk>\d+)/$', views.EventDetail.as_view(),
+        name='event-detail'),
 ]
--- countryx/events/views.py
+++ countryx/events/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Event
 
@@ -11,3 +11,7 @@ class ListEvents(StaffOnlyMixin, ListView):
 
     def get_queryset(self):
         return Event.objects.all().order_by('-timestamp')
+
+
+class EventDetail(StaffOnlyMixin, DetailView):
+    model = Event
