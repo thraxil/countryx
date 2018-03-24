40d61ac8b0a3002df1701a424d787e9079b83a60
Anders Pearson <anders@thraxil.org>
2018-03-24 19:28:25 +0000

    Revert "thraxilsettings"
    This reverts commit d8336bc06264077b7c0dd999f250fcc5867cec30.

--- countryx/settings_docker.py
+++ countryx/settings_docker.py
@@ -1,17 +1,98 @@
 # flake8: noqa
 from .settings_shared import *
 import os
-import os.path
 
-app = 'countryx'
-base = os.path.dirname(__file__)
+# required settings:
+SECRET_KEY = os.environ['SECRET_KEY']
 
-locals().update(
-    common(
-        app=app,
-        base=base,
-        celery=False,
-        INSTALLED_APPS=INSTALLED_APPS,
-        STATIC_ROOT=STATIC_ROOT,
-        MIDDLEWARE=MIDDLEWARE,
-    ))
+# optional/defaulted settings
+DB_NAME = os.environ.get('DB_NAME', 'countryx')
+DB_HOST = os.environ.get(
+    'DB_HOST',
+    os.environ.get('POSTGRESQL_PORT_5432_TCP_ADDR', ''))
+DB_PORT = int(
+    os.environ.get(
+        'DB_PORT',
+        os.environ.get('POSTGRESQL_PORT_54342_TCP_PORT', 5432)))
+DB_USER = os.environ.get('DB_USER', '')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
+
+AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', '')
+AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', '')
+AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY = AWS_SECRET_KEY
+
+if 'ALLOWED_HOSTS' in os.environ:
+    ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
+
+TIME_ZONE = os.environ.get('TIME_ZONE', 'America/New_York')
+
+EMAIL_HOST = os.environ.get(
+    'EMAIL_HOST',
+    os.environ.get('POSTFIX_PORT_25_TCP_ADDR', 'localhost'))
+EMAIL_PORT = os.environ.get(
+    'EMAIL_PORT',
+    os.environ.get('POSTFIX_PORT_25_TCP_PORT', 25))
+
+SERVER_EMAIL = os.environ.get('SERVER_EMAIL', "countryx@ccnmtl.columbia.edu")
+
+# -------------------------------------------
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), "templates"),
+)
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : DB_NAME,
+        'HOST' : DB_HOST,
+        'PORT' : DB_PORT,
+        'USER' : DB_USER,
+        'PASSWORD' : DB_PASSWORD,
+        'ATOMIC_REQUESTS': True,
+        }
+}
+
+if AWS_S3_CUSTOM_DOMAIN:
+    AWS_PRELOAD_METADATA = True
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+    # static data, e.g. css, js, etc.
+    STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+    STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+    COMPRESS_ENABLED = True
+    COMPRESS_OFFLINE = True
+    COMPRESS_ROOT = STATIC_ROOT
+    COMPRESS_URL = STATIC_URL
+    COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+}
+OPBEAT_ORGANIZATION_ID=os.environ.get('OPBEAT_ORGANIZATION_ID')
+OPBEAT_APP_ID=os.environ.get('OPBEAT_APP_ID')
+OPBEAT_SECRET_TOKEN=os.environ.get('OPBEAT_SECRET_TOKEN')
+if OPBEAT_ORGANIZATION_ID:
+    INSTALLED_APPS += [
+        'opbeat.contrib.django',
+    ]
+    OPBEAT = {
+        'ORGANIZATION_ID': OPBEAT_ORGANIZATION_ID,
+        'APP_ID': OPBEAT_APP_ID,
+        'SECRET_TOKEN': OPBEAT_SECRET_TOKEN,
+    }
+    MIDDLEWARE.insert(0, 'opbeat.contrib.django.middleware.OpbeatAPMMiddleware')
+
+RAVEN_DSN = os.environ.get('RAVEN_DSN', None)
+
+if RAVEN_DSN:
+    RAVEN_CONFIG = {
+        'dsn': RAVEN_DSN,
+    }
--- countryx/settings_shared.py
+++ countryx/settings_shared.py
@@ -1,21 +1,146 @@
 # Django settings for countryx project.
 import os.path
-from thraxilsettings.shared import common
+import sys
 
-app = 'countryx'
-base = os.path.dirname(__file__)
+DEBUG = True
 
-locals().update(common(app=app, base=base))
+ADMINS = (
+    ('CCNMTL', 'ccnmtl-sysadmin@columbia.edu'),
+)
+
+MANAGERS = ADMINS
+
+ALLOWED_HOSTS = [".ccnmtl.columbia.edu", "localhost"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'countryx',
+        'HOST': '',
+        'PORT': 5432,
+        'USER': '',
+        'PASSWORD': '',
+        'ATOMIC_REQUESTS': True,
+    }
+}
+
+if 'test' in sys.argv or 'jenkins' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+            'HOST': '',
+            'PORT': '',
+            'USER': '',
+            'PASSWORD': '',
+            'ATOMIC_REQUESTS': True,
+        }
+    }
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+TEST_OUTPUT_DIR = 'reports'
 
 # PROJECT_APPS = ['countryx.sim', 'countryx.events']
 
-INSTALLED_APPS += [  # noqa
+USE_TZ = True
+TIME_ZONE = 'America/New_York'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = False
+MEDIA_ROOT = "/var/www/countryx/uploads/"
+MEDIA_URL = '/uploads/'
+STATIC_URL = '/media/'
+SECRET_KEY = 'dummy-)ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), "templates"),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
+    'django_statsd.middleware.GraphiteMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'waffle.middleware.WaffleMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+ROOT_URLCONF = 'countryx.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
     'bootstrapform',
     'countryx.sim',
     'countryx.events',
     'countryx.reports',
+    'django_statsd',
+    'waffle',
     'impersonate',
+    'django_markwhat',
+    'compressor',
+    'debug_toolbar',
+    'raven.contrib.django.raven_compat',
 ]
+
+INTERNAL_IPS = ('127.0.0.1', )
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+)
+
+STATSD_CLIENT = 'statsd.client'
+STATSD_PREFIX = 'countryx'
+STATSD_HOST = 'localhost'
+STATSD_PORT = 8125
+
+MEDIA_URL = "/uploads/"
+MEDIA_ROOT = 'uploads'
+STATIC_URL = "/media/"
+STATIC_ROOT = "/tmp/countryx/static"
+STATICFILES_DIRS = ('media/',)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+COMPRESS_URL = "/media/"
+COMPRESS_ROOT = "media/"
+AWS_QUERYSTRING_AUTH = False
+
+THUMBNAIL_SUBDIR = "thumbs"
+EMAIL_SUBJECT_PREFIX = "[countryx] "
+EMAIL_HOST = 'localhost'
+SERVER_EMAIL = "countryx@ccnmtl.columbia.edu"
 
 STATE_COLORS = [
     'ffd478', '009192', 'ff9400', 'd25700', '935200', 'd4fb79',
@@ -25,3 +150,11 @@ STATE_COLORS = [
     'fffc00', '76d6ff', '00f900', '929292', '929000']
 
 LOGIN_REDIRECT_URL = "/"
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+SESSION_COOKIE_HTTPONLY = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+}
