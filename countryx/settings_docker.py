26e674c24c704c4147100020134e7f4cf58db0fa
Anders Pearson <anders@thraxil.org>
2018-03-24 19:54:08 +0000

    bring it in here

--- countryx/settings_docker.py
+++ countryx/settings_docker.py
@@ -1,6 +1,19 @@
 # flake8: noqa
 from .settings_shared import *
 import os
+import os.path
+
+from thraxilsettings.docker import common
+
+locals().update(
+    common(
+        app=app,
+        base=base,
+        celery=False,
+        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,
+        MIDDLEWARE=MIDDLEWARE,
+    ))
 
 # required settings:
 SECRET_KEY = os.environ['SECRET_KEY']
